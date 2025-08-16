51648abed0648825a3c6429be721c3b5dc534b41
Julio Andres Casanova Ramirez <casanovaramirezj@gmail.com>
2025-08-16 11:05:44 -0500

    categoryB

---
+++ pruebas/1.py
@@ -0,0 +1,9 @@
+# import edge_tts
+# import asyncio
+
+# async def descar_sonidos(texto):
+#     voice = "en-US-GuyNeural"
+#     communicate = edge_tts.Communicate(texto, voice)
+#     await communicate.save("output.mp3")
+
+# asyncio.run(descar_sonidos("AGREEMENT"))
